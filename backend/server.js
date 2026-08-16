78130fc92083120575b05e97073234741c0055a0
sgreen5168 <126698244+sgreen5168@users.noreply.github.com>
2026-08-16 12:13:55 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: '50mb' }));
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const jobs = new Map();
 
 async function updateJob(id, updates) {
